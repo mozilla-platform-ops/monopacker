cbf7dafe4919126a69b1d8fb411e3e9ac3e0c5af
Miles Crabill <mcrabill@mozilla.com>
2019-08-08 12:53:59 -0700

    add scripts to install tc-worker-runner and docker-worker

---
+++ scripts/default/60-worker-runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -exv
+
+# init helpers
+helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
+. ${helpers_dir}/*.sh
+
+# install and configure taskcluster-worker-runner
+worker_runner_version="v0.3.2"
+retry curl -L -o /usr/local/bin/start-worker https://github.com/taskcluster/taskcluster-worker-runner/releases/download/${worker_runner_version}/start-worker-linux-amd64
+file /usr/local/bin/start-worker
+chmod +x /usr/local/bin/start-worker
--- scripts/docker-worker/60-docker-worker.sh
+++ scripts/docker-worker/60-docker-worker.sh
@@ -6,4 +6,41 @@ set -exv
 helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
 . ${helpers_dir}/*.sh
 
-# TODO
+# download docker-worker to /home/ubuntu/docker-worker
+docker_worker_version="v201908071906"
+retry curl -L -o /tmp/docker-worker.tgz "https://github.com/taskcluster/docker-worker/archive/${docker_worker_version}.tar.gz"
+mkdir -p /home/ubuntu/docker-worker
+tar xvf /tmp/docker-worker.tgz -C /home/ubuntu/docker-worker --strip-components 1
+
+cat << EOF > /usr/local/bin/start-docker-worker
+#!/bin/bash
+set -exv
+/usr/local/bin/start-worker /etc/worker-runner/start-worker.yml 2>&1 | logger --tag docker-worker
+EOF
+file /usr/local/bin/start-docker-worker
+chmod +x /usr/local/bin/start-docker-worker
+
+# worker runner config
+mkdir -p /etc/worker-runner
+cat << EOF > /etc/worker-runner/start-worker.yml
+provider:
+    providerType: aws-provisioner
+worker:
+    implementation: docker-worker
+    path: /home/ubuntu/docker-worker
+    configPath: /home/ubuntu/worker.cfg
+EOF
+
+cat << EOF > /etc/systemd/system/docker-worker.service
+[Unit]
+Description=Taskcluster docker worker
+After=docker.service
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/start-docker-worker /etc/worker-runner/start-worker.yml 2>&1 | logger --tag docker-worker
+User=root
+
+[Install]
+RequiredBy=multiuser.target
+EOF
