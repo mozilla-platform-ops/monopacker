09c36900f4f913ee879290e8169ae4b9d87bd87e
Miles Crabill <mcrabill@mozilla.com>
2019-08-08 15:57:12 -0700

    reformat versions for sidecars

--- scripts/docker-worker/15-sidecars.sh
+++ scripts/docker-worker/15-sidecars.sh
@@ -9,14 +9,16 @@ helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
 docker --version
 
 # Versions for sidecar containers
-relengapi_proxy_version=2.3.1
-taskcluster_proxy_version=5.1.0
-livelog_version=4
-dind_service_version=4.0
-worker_runner_version=0.3.0
 
 # Pull images used for sidecar containers
+taskcluster_proxy_version=5.1.0
 retry docker pull taskcluster/taskcluster-proxy:$taskcluster_proxy_version
+
+livelog_version=4
 retry docker pull taskcluster/livelog:v$livelog_version
+
+dind_service_version=4.0
 retry docker pull taskcluster/dind-service:v$dind_service_version
+
+relengapi_proxy_version=2.3.1
 retry docker pull taskcluster/relengapi-proxy:$relengapi_proxy_version
