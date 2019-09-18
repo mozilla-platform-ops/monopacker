8bb63c5047fec29b405b44ca821f14619f24a7e7
Brian Stack <bstack@mozilla.com>
2019-09-17 17:02:10 -0700

    Actually make papertrail work now

--- scripts/worker-runner-linux/20-set-up-papertrail.sh
+++ scripts/worker-runner-linux/20-set-up-papertrail.sh
@@ -6,7 +6,23 @@ set -exv
 helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
 . ${helpers_dir}/*.sh
 
-cat << EOF > /etc/rsyslog.d/0-docker-worker.conf
+# Get recent CA bundle for papertrail
+retry curl -s -o /etc/papertrail-bundle.pem https://papertrailapp.com/tools/papertrail-bundle.pem
+md5=`md5sum /etc/papertrail-bundle.pem | awk '{ print $1 }'`
+if [ "$md5" != "2c43548519379c083d60dd9e84a1b724" ]; then
+    echo "md5 for papertrail CA bundle does not match"
+    exit -1
+fi
+
+retry apt install -y rsyslog-gnutls
+
+echo "Setting +x and importing papertrail host"
+set +x
+export PAPERTRAIL_HOST=$(cat /etc/taskcluster/secrets/worker_papertrail_dest)
+set -x
+echo "Resuming -x"
+
+cat << EOF > /etc/rsyslog.d/0-taskcluster.conf
 \$DefaultNetstreamDriverCAFile /etc/papertrail-bundle.pem # trust these CAs
 \$ActionSendStreamDriver gtls # use gtls netstream driver
 \$ActionSendStreamDriverMode 1 # require TLS
@@ -24,5 +40,5 @@ cat << EOF > /etc/rsyslog.d/0-docker-worker.conf
 \$ActionQueueSaveOnShutdown on
 \$ActionQueueTimeoutEnqueue 10
 \$ActionQueueDiscardSeverity 0
-*.* @@$PAPERTRAIL
+*.* @@$PAPERTRAIL_HOST
 EOF
