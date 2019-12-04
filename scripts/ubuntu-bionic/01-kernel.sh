19a3210bda34d18d4ed77a512d624a9c36aa80f0
Wander Lairson Costa <wcosta@mozilla.com>
2019-12-04 19:58:16 -0300

    Support AWS metal instances
    This add a custom AWS and v4l2loopback driver that supports up to 96
    devices.

--- scripts/ubuntu-bionic/01-kernel.sh
+++ scripts/ubuntu-bionic/01-kernel.sh
@@ -14,7 +14,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Update kernel
 # We install the generic kernel because it has the V4L2 driver
-KERNEL_VERSION=4.15.0-58-generic
+KERNEL_NOVARIANT_VERSION=4.15.0-1056
+KERNEL_VERSION=$KERNEL_NOVARIANT_VERSION-aws
 
 # testing updating AWS kernel and adding extras package
 # KERNEL_VERSION=4.15.0-1045-aws
@@ -30,15 +31,31 @@ unattended-upgrade
 apt remove -y $(ls -1 /boot/vmlinuz-*{aws,gcp} | sed -e 's,/boot/vmlinuz,linux-image,')
 apt autoremove -y --purge
 
+# necessary for linux-modules-extra
+retry apt install -y crda wireless-crda
+
 # install new kernel
+pushd /var/kernel
+dpkg -i --force-confnew linux-modules-$KERNEL_VERSION*.deb
+dpkg -i --force-confnew linux-image-$KERNEL_VERSION*.deb
+dpkg -i --force-confnew linux-modules-extra-$KERNEL_VERSION*.deb
+dpkg -i --force-confnew linux-aws-headers-$KERNEL_NOVARIANT_VERSION*.deb
+dpkg -i --force-confnew linux-headers-$KERNEL_VERSION*.deb
+dpkg -i --force-confnew linux-buildinfo-$KERNEL_VERSION*.deb
+popd
+
+# Avoid kernel upgrades
+echo linux-image-$KERNEL_VERSION hold | dpkg --set-selections
+echo linux-modules-$KERNEL_VERSION hold | dpkg --set-selections
+echo linux-modules-extra-$KERNEL_VERSION hold | dpkg --set-selections
+echo linux-aws-headers-$KERNEL_NOVARIANT_VERSION hold | dpkg --set-selections
+echo linux-headers-$KERNEL_VERSION hold | dpkg --set-selections
+echo linux-buildinfo-$KERNEL_VERSION hold | dpkg --set-selections
+echo linux-image-aws hold | dpkg --set-selections
+echo linux-aws hold | dpkg --set-selections
+
 # install crash debug tools
-retry apt install -y \
-    linux-image-$KERNEL_VERSION \
-    linux-modules-$KERNEL_VERSION \
-    linux-modules-extra-$KERNEL_VERSION \
-    linux-headers-$KERNEL_VERSION \
-    linux-crashdump \
-    kmod
+retry apt install -y linux-crashdump kmod
 
 # kernel debug
 grep 'USE_KDUMP' /etc/default/kdump-tools
