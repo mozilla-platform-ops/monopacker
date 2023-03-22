8666db30aebf26c5b1cdf52b4c13e9b7752ead7a
Andrew Erickson <aerickson@mozilla.com>
2023-03-22 18:12:15 -0400

    cleanup, comments

--- scripts/worker-runner-gw-systemd/10-create-worker-runner-config.sh
+++ scripts/worker-runner-gw-systemd/10-create-worker-runner-config.sh
@@ -30,8 +30,6 @@ worker_runner_config="/etc/start-worker.yml"
 # taken from https://github.com/mozilla-platform-ops/ronin_puppet/blob/master/modules/linux_generic_worker/templates/worker-runner-config.yml.erb
 
 cat << EOF > "${worker_runner_config}"
-cacheOverRestarts: true
-getSecrets: false
 provider:
     providerType: ${CLOUD}
 worker:
