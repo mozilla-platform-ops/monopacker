f9730dc059622a0301f932489a199dc56c19ca41
Andrew Erickson <aerickson@mozilla.com>
2023-03-23 18:03:46 -0400

    cuda: install via network

--- scripts/ubuntu-cuda/10-install-cuda-libs.sh
+++ scripts/ubuntu-cuda/10-install-cuda-libs.sh
@@ -14,5 +14,11 @@ done
 
 # install cuda from runfile
 #   from https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=runfile_local
-wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run 
-sh cuda_12.1.0_530.30.02_linux.run
+# wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run 
+# sh cuda_12.1.0_530.30.02_linux.run
+
+# install cuda from network
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda
