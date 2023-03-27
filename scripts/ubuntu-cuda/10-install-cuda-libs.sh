4550534012192cb4d0aafc7fc1e7fa42cb9eb428
Andrew Erickson <aerickson@mozilla.com>
2023-03-27 13:35:41 -0400

    more cleanup

--- scripts/ubuntu-cuda/10-install-cuda-libs.sh
+++ scripts/ubuntu-cuda/10-install-cuda-libs.sh
@@ -12,10 +12,6 @@ done
 # prepare
 #
 
-# install fails without (on minimal ubuntu)
-# sudo apt update
-# sudo apt install -y libxml2
-
 # problem: broken symlinks in directory below
 #   ls -la ls /usr/src/linux-headers-5.15.0-1030-gcp
 # fix:
@@ -29,21 +25,8 @@ sudo apt-get -y reinstall linux-headers-`uname -r` linux-gcp-headers-`uname -r |
 # install driver
 #
 
-# attempt 1: failed due to asking for input
-# install cuda from runfile
-#   from https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=22.04&target_type=runfile_local
-# wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run 
-# sh cuda_12.1.0_530.30.02_linux.run
-
-# attempt 2
-# install cuda from network
+# install cuda from network (runfile demands input and fails)
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
-# set DEBIAN_FRONTEND=noninteractive despite being set already in 'ubuntu-jammy' script?!?
-# DEBIAN_FRONTEND=noninteractive \
 sudo apt-get -y install cuda
-
-# fix dkms issues (worked on started instance... figure out ordering)
-# - header dir was full of broken symlinks for some reason... !?!?
-# sudo dkms autoinstall
