c3e082e744b7e038d1b3547e7b737389ff1ec04f
Andrew Erickson <aerickson@mozilla.com>
2023-04-27 14:20:03 -0700

    install cuda keyring from /tmp

--- scripts/ubuntu-cuda/10-install-cuda-libs.sh
+++ scripts/ubuntu-cuda/10-install-cuda-libs.sh
@@ -26,6 +26,7 @@ sudo apt-get -y reinstall linux-headers-`uname -r` linux-gcp-headers-`uname -r |
 #
 
 # install cuda from network (runfile demands input and fails)
+cd /tmp
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
