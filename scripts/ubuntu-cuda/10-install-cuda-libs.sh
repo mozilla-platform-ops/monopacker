8305351fc9a7af96aa81d6ea653221d815d9ba30
Andrew Erickson <aerickson@mozilla.com>
2023-04-27 14:49:15 -0700

    header name change fixes

--- scripts/ubuntu-cuda/10-install-cuda-libs.sh
+++ scripts/ubuntu-cuda/10-install-cuda-libs.sh
@@ -19,7 +19,26 @@ done
 #
 # ensure kernel headers are present so dkms works
 # - had issue where there were broken symlinks
-sudo apt-get -y reinstall linux-headers-`uname -r` linux-gcp-headers-`uname -r | sed -r  s/-gcp//`
+
+# fix 1:
+#  not needed now? package name changed
+#      linux-gcp-headers-`uname -r | sed -r  s/-gcp//`
+
+# fix 2
+# now: linux-gcp-5.19-headers-5.19.0-1021
+# `uname -r`: 5.19.0-1021-gcp
+version=`uname -r`
+version_minus_dash_gcp=`uname -r | sed -r  s/-gcp//`
+short_version=`uname -r | cut -d "." -f1,2`
+pkg_name="linux-gcp-${short_version}-headers-${version_minus_dash_gcp}"
+
+sudo apt-get update
+sudo apt-get -y reinstall linux-headers-gcp linux-headers-`uname -r` ${pkg_name}
+
+# TODO: install future kernel headers (latest in `dpkg --list 'linux-image*'` output)
+#       in addition to current (uname -r)?
+#         - not needed as we're compiling on what's running?
+
 
 #
 # install driver
