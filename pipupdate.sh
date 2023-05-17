1db919177d0202641c8d43ece2a2b0a518cc7d8a
Andrew Erickson <aerickson@mozilla.com>
2023-05-17 06:15:48 -0700

    switch to Poetry (#107)

--- pipupdate.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-export CUSTOM_COMPILE_COMMAND=./pipupdate.sh
-# See https://stackoverflow.com/questions/58843905/what-is-the-proper-way-to-decide-whether-to-allow-unsafe-package-versions-in-pip
-# for an explanation of why we use --allow-unsafe option below
-python -m piptools compile --generate-hashes "${@}" --allow-unsafe --output-file requirements.txt
