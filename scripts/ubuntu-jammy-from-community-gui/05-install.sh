4befd55450169b308280451d7148468dab812fe6
Andrew Erickson <aerickson@mozilla.com>
2023-12-28 12:36:23 -0600

    ordering fix, comment

--- scripts/ubuntu-jammy-from-community-gui/05-install.sh
+++ scripts/ubuntu-jammy-from-community-gui/05-install.sh
@@ -10,6 +10,9 @@ done
 
 # taken from https://github.com/taskcluster/community-tc-config/blob/main/imagesets/generic-worker-ubuntu-22-04/bootstrap.sh
 
+# AJE added
+export DEBIAN_FRONTEND=noninteractive 
+
 # place a new worker unit file that is required by the graphical target
 cat > /lib/systemd/system/worker.service << EOF
 [Unit]
@@ -28,6 +31,9 @@ User=root
 RequiredBy=graphical.target
 EOF
 
+# podman installed in non-gui
+retry apt-get install -y ubuntu-desktop ubuntu-gnome-desktop
+
 # Installs the v4l2loopback kernel module
 # used for the video device, and vkms
 # required by Wayland
@@ -37,9 +43,6 @@ grep -Fx vkms /etc/modules || echo vkms >> /etc/modules
 # disable udev rule that tags platform-vkms with "mutter-device-ignore"
 # ENV{ID_PATH}=="platform-vkms", TAG+="mutter-device-ignore"
 sed '/platform-vkms/d' /lib/udev/rules.d/61-mutter.rules > /etc/udev/rules.d/61-mutter.rules
-
-# podman installed in non-gui
-retry apt-get install -y ubuntu-desktop ubuntu-gnome-desktop
 
 # vnc configuration omitted
 # - see https://github.com/taskcluster/community-tc-config/blob/5431d9f72f52eeb2bb232dcac55ad399f747ac6a/imagesets/generic-worker-ubuntu-22-04-staging/bootstrap.sh
