5778bb02de55aeefb647c905b656f8ee4cab5c2f
Andrew Erickson <aerickson@mozilla.com>
2024-01-18 18:27:03 -0500

    install zstandard pip

---
+++ scripts/ubuntu-jammy-from-community/15-additional-pips.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -exv
+
+# init helpers
+helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
+for h in ${helpers_dir}/*.sh; do
+    . $h;
+done
+
+# add additional pips (that don't have apt packages)
+
+pip3 install zstandard
