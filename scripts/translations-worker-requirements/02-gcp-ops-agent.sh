c648df4a62fc3a97ecc1bfd22666d8855bc34482
Ben Hearsum <ben@mozilla.com>
2024-01-19 09:27:05 -0500

    Install GCP Ops Agent to translations GPU worker image

---
+++ scripts/translations-worker-requirements/02-gcp-ops-agent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -exv
+
+# init helpers
+helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
+for h in ${helpers_dir}/*.sh; do
+    . $h;
+done
+
+retry curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+bash add-google-cloud-ops-agent-repo.sh --also-install
