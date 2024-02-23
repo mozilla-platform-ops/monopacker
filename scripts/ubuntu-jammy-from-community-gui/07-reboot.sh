a6cb2c8863daec533fd3329bffd805c747fb63b0
Andrew Erickson <aerickson@mozilla.com>
2024-02-23 13:52:08 -0500

    try to get rebooting working

---
+++ scripts/ubuntu-jammy-from-community-gui/07-reboot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -exv
+
+# init helpers
+helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
+for h in ${helpers_dir}/*.sh; do
+    . $h;
+done
+
+#
+# reboot to use the new kernel
+#
+# Shutdown and wait forever; packer will consider this script to have finished and
+# start on the next script when it reconnects
+shutdown -r now
+sleep 60
