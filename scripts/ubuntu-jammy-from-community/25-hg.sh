5869e29c48bc72e8d1501906371b1a807ca73983
Andrew Erickson <aerickson@mozilla.com>
2024-03-11 12:16:49 -0700

    2204 images: install hg extensions (#130)

---
+++ scripts/ubuntu-jammy-from-community/25-hg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -exv
+
+# init helpers
+helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
+for h in ${helpers_dir}/*.sh; do
+    . $h;
+done
+
+# hg
+
+#
+# setup robustcheckout
+#
+
+RC_PLUGIN="/etc/hgext/robustcheckout.py"
+RC_URL="https://hg.mozilla.org/hgcustom/version-control-tools/raw-file/tip/hgext/robustcheckout/__init__.py"
+
+mkdir /etc/hgext
+chmod 0755 /etc/hgext
+chown -R root:root /etc/hgext
+
+curl -o $RC_PLUGIN $RC_URL
+chmod 0644 $RC_PLUGIN
+
+cat << EOF > /etc/mercurial/hgrc
+[extensions]
+robustcheckout = $RC_PLUGIN
+EOF
+
+# test
+hg --version
+hg showconfig | grep robustcheckout
+hg robustcheckout --help
