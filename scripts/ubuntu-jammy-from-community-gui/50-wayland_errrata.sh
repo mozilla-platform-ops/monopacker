b945d1081419da58899a9191777a76c38e19b2ff
Andrew Erickson <aerickson@mozilla.com>
2024-04-17 14:20:48 -0700

    comment cleanup, alissy's recommended changes

--- scripts/ubuntu-jammy-from-community-gui/50-wayland_errrata.sh
+++ scripts/ubuntu-jammy-from-community-gui/50-wayland_errrata.sh
@@ -23,25 +23,13 @@ apt install -y crudini
 # dconf settings
 #
 
-# /etc/dconf/profile/user should have (no leading spaces):
-#
-#   user-db:user
-#   system-db:local
 cat > /etc/dconf/profile/user << EOF
 user-db:user
 system-db:local
 EOF
 
-# /etc/dconf/db/local.d/00-tc-gnome-settings should have (no leading spaces): 
-#
-# # /org/gnome/desktop/session/idle-delay
-# [org/gnome/desktop/session]
-# idle-delay=uint32 0
-#
-# # /org/gnome/desktop/lockdown/disable-lock-screen
-# [org/gnome/desktop/lockdown]
-# disable-lock-screen=true
 mkdir /etc/dconf/db/local.d/
+# dconf user settings
 cat > /etc/dconf/db/local.d/00-tc-gnome-settings << EOF
 # /org/gnome/desktop/session/idle-delay
 [org/gnome/desktop/session]
@@ -75,23 +63,19 @@ echo "----"
 grep 'XorgEnable' /etc/gdm3/custom.conf
 grep 'XorgEnable' /etc/gdm3/custom.conf | grep false
 
+
 #
-# gdm3 service file
+# gdm wait service file
 #
 
-# copy /lib/systemd/system/gdm3.service to /etc/systemd/system and change its 
-# ExecStartPre to `/bin/sleep 15`
+cat > /etc/systemd/system/gdm.service.d/gdm-wait.conf << EOF
+[Unit]
+Description=Extra 30s wait
 
-# NOTES: this is a hack. alissy is working on a better solution.
+[Service]
+ExecStartPre=/bin/sleep 30
+EOF
 
-cp /lib/systemd/system/gdm3.service /etc/systemd/system/gdm3.service
-crudini --set /etc/systemd/system/gdm3.service Service ExecStartPre '/bin/sleep 15'
-
-# verify/test
-cat /etc/systemd/system/gdm3.service
-echo "----"
-grep 'ExecStartPre' /etc/systemd/system/gdm3.service
-grep 'ExecStartPre' /etc/systemd/system/gdm3.service | grep 'sleep 15'
 
 #
 # extra packages
@@ -105,7 +89,31 @@ apt install -y ubuntu-restricted-extras wl-clipboard
 
 
 #
-# TODO: generate mutter's monitors.xml
+# write mutter's monitors.xml
 #
 
-# config TBD
+cat > /etc/xdg/monitors.xml << EOF
+<monitors version="2">
+  <configuration>
+    <logicalmonitor>
+      <x>0</x>
+      <y>0</y>
+      <scale>1</scale>
+      <primary>yes</primary>
+      <monitor>
+        <monitorspec>
+          <connector>Virtual-1</connector>
+          <vendor>unknown</vendor>
+          <product>unknown</product>
+          <serial>unknown</serial>
+        </monitorspec>
+        <mode>
+          <width>1920</width>
+          <height>1080</height>
+          <rate>60.000</rate>
+        </mode>
+      </monitor>
+    </logicalmonitor>
+  </configuration>
+</monitors>
+EOF
