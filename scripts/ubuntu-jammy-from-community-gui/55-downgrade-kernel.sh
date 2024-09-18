8288e0c039467a93a227331e199d323fe53c77f4
Andrew Erickson <aerickson@mozilla.com>
2024-09-18 00:15:31 -0400

    try downgrading kernel

---
+++ scripts/ubuntu-jammy-from-community-gui/55-downgrade-kernel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -exv
+
+# init helpers
+helpers_dir=${MONOPACKER_HELPERS_DIR:-"/etc/monopacker/scripts"}
+for h in ${helpers_dir}/*.sh; do
+    . $h;
+done
+
+#
+
+sudo apt remove linux-image-6.8.0-1015-gcp
